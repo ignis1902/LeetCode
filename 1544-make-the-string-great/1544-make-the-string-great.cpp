50fee641ac30760931eacdbe23de60a04ea13492
ignis1902 <adeshrm267@gmail.com>
2026-09-25 23:43:12 +0530

    Time: 0 ms (100%), Space: 8.7 MB (49.58%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string makeGood(string s) {
+
+        stack<char> st;
+
+        for(auto x : s){
+
+            if(!st.empty() && abs(st.top() - x) == 32){
+                st.pop();
+            }
+            else{
+                st.push(x);
+            }
+        }
+
+        string ans;
+
+        while(!st.empty()){
+            ans += st.top();
+            st.pop();
+        }
+
+        reverse(ans.begin(), ans.end());
+
+        return ans;
+    }
+};
